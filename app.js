912baa4b101f03a79a97732524b0c9d94647fec7
Cody Shaben <codyshaben@Codys-MacBook-Air.local>
2019-11-26 14:39:47 -0700

    added environment to app.js file

--- app.js
+++ app.js
@@ -2,14 +2,17 @@ const express = require('express');
 const cors = require('cors')
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const Knex = require('knex')
 const { Model } = require('objection')
-const knexConfig = require('./knexfile')
 const users = require('./controllers/users')
 const projects = require('./controllers/projects')
 const app = express();
 
-const knex = Knex(knexConfig.production)
+const environment = process.env.NODE_ENV || 'development'
+const knexConfig = require('./knexfile')
+const environmentConfig = knexConfig[environment]
+const Knex = require('knex')
+
+const knex = Knex(environmentConfig)
 
 Model.knex(knex)
 
