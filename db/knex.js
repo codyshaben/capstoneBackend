725161e1d0ad355773406b8189bac939dbfef0d5
Cody Shaben <codyshaben@Codys-MacBook-Air.local>
2019-11-27 13:05:29 -0700

    changed around knex.js file

--- db/knex.js
+++ db/knex.js
@@ -1,11 +1,12 @@
 const environment = process.env.NODE_ENV || 'development';
-const config = require('../knexfile.js')[environment];
-
+const config = require('../knexfile.js')
+const environmentConfig = config[environment]
 const objection = require('objection')
-const Model = objection.Model
-const knex = require('knex')
-const connection = knex(config)
+const { Model } = objection.Model
+const Knex = require('knex')
+const connection = Knex(environmentConfig)
 
-Model(connection)
+Model.knex(connection)
 
-module.exports = Model(connection)
+module.exports = Model.knex(connection)
+
